8ff1c72927efa41025cd24bb227b526d4ef9dfe0
Alvin Fadli <alvinfdl21@gmail.com>
2023-05-31 04:31:22 +0700

    update homepage line-chart

--- lib/screens/cashier/cashier_homepage.dart
+++ lib/screens/cashier/cashier_homepage.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:nita_grocers/screens/cashier/cashier_history.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'list_product.dart';
 import '../widgets/bottom_navigation.dart';
 
@@ -10,6 +9,30 @@ class cashierHomepage extends StatefulWidget {
   const cashierHomepage({Key? key}) : super(key: key);
   @override
   _CashierHomepageState createState() => _CashierHomepageState();
+}
+
+class LineChartWidget extends StatelessWidget {
+  final List<charts.Series<num, int>> seriesList;
+  final bool animate;
+
+  LineChartWidget(this.seriesList, {required this.animate});
+
+  @override
+  Widget build(BuildContext context) {
+    return charts.LineChart(
+      seriesList,
+      animate: animate,
+      defaultRenderer: charts.LineRendererConfig(
+        includeArea: true,
+        stacked: false,
+      ),
+      primaryMeasureAxis: charts.NumericAxisSpec(
+        tickProviderSpec: charts.BasicNumericTickProviderSpec(
+          desiredTickCount: 5,
+        ),
+      ),
+    );
+  }
 }
 
 class _CashierHomepageState extends State<cashierHomepage> {
@@ -39,6 +62,24 @@ class _CashierHomepageState extends State<cashierHomepage> {
 
   @override
   Widget build(BuildContext context) {
+    final data = [
+      {'x': 0, 'y': 5},
+      {'x': 1, 'y': 10},
+      {'x': 2, 'y': 15},
+      {'x': 3, 'y': 12},
+      {'x': 4, 'y': 8},
+    ];
+
+    final series = [
+      charts.Series<num, int>(
+        id: 'Line Chart',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (num point, _) => point.toInt(),
+        measureFn: (num point, _) => point.toDouble(),
+        data: data.map((point) => point['y'] as num).toList(),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -61,6 +102,7 @@ class _CashierHomepageState extends State<cashierHomepage> {
                 Container(
                   height: 200,
                   //child: buildLineChart(generateChartData()),
+                  child: LineChartWidget(series, animate: true),
                 ),
               ],
             ),
@@ -71,13 +113,13 @@ class _CashierHomepageState extends State<cashierHomepage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Container 2',
+                  'Penjualan Hari Ini',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 8),
                 Container(
                   height: 200,
-                  color: Colors.blue,
+                  color: Colors.white,
                   child: Center(
                     child: Text(
                       'Container 2',
@@ -94,13 +136,13 @@ class _CashierHomepageState extends State<cashierHomepage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Container 3',
+                  'Produk Terlaris',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 8),
                 Container(
                   height: 200,
-                  color: Colors.green,
+                  color: Colors.white,
                   child: Center(
                     child: Text(
                       'Container 3',
